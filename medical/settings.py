a112cc73e23e87358d4c8e166c690f830cc20cd7
Brijeshvekariya <bvekariya54@gmail.com>
2023-11-25 18:33:57 +0530

    updated

--- medical/settings.py
+++ medical/settings.py
@@ -133,3 +133,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'bvekariya54@gmail.com'
+EMAIL_HOST_PASSWORD = "dlgp nhre oecj qsry"
