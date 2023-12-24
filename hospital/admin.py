c58bd00bbabb12fb4c51fa01e26122b195200db6
Brijeshvekariya <bvekariya54@gmail.com>
2023-12-24 17:20:39 +0530

    updated

--- hospital/admin.py
+++ hospital/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from . models import Contact,User,Doctor,appointment,patient_profile
 
+
+
+
+
 # Register your models here.
 admin.site.register(Contact)
 admin.site.register(User)
