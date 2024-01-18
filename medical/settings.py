8f2c73362ee21a8f83df9b29510a7113308aebfa
Brijeshvekariya <bvekariya54@gmail.com>
2024-01-18 19:15:28 +0530

    updated

--- medical/settings.py
+++ medical/settings.py
@@ -123,8 +123,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATIC_ROOT = BASE_DIR / 'static'
-# STATICFILES_DIRS = [
-#     BASE_DIR / "static",
+STATICFILES_DIRS = [BASE_DIR / "static"]
 #     "/var/www/static/",
 # ]
 
--- medical/urls.py
+++ medical/urls.py
@@ -18,13 +18,16 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
-    path('dj_admin/', admin.site.urls),
-    path('admin/',include('adminapp.urls')),
+    path('admin/', admin.site.urls),
+    # path('admin/',include('adminapp.urls')),
     path('',include('hospital.urls')),
 ]
 
 if settings.DEBUG:
         urlpatterns += static(settings.MEDIA_URL,
                               document_root=settings.MEDIA_ROOT)
+        
+urlpatterns += staticfiles_urlpatterns()
